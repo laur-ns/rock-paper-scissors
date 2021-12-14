4a351d7b1a1c27b3c0d37f06547f7bb905a97864
Laurens <laurens-morales@outlook.com>
2021-12-15 02:46:45 +1300

    Forgot to make commits but everything is done

--- rps.js
+++ rps.js
@@ -9,16 +9,50 @@ function computerPlay() {
   else { return "Scissors"; }
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (player === computerSelection) {
+function playRound(player, computer) {
+  if (player === computer) {
+    title.textContent = `That's a draw. You both picked ${player}!`;
+    console.log(player);
+    console.log(computer);
     return "Draw";
   }
-  else if (player === "Rock" && computerSelection === "Scissors" ||
-           player === "Paper" && computerSelection === "Rock"    ||
-           player === "Scissors" && computerSelection === "Paper")
-          { return "Win"; }
+  else if (player === "Rock" && computer === "Scissors" ||
+           player === "Paper" && computer === "Rock"    ||
+           player === "Scissors" && computer === "Paper")
+          { title.textContent = `You won this round. ${player} beats ${computer}!`;
+            return "Win"; }
   else {
+    title.textContent = `You lost this round. ${computer} beats ${player}!`;
     return "Lose";
   }
 }
+
+const winText = document.querySelector('div .win');
+const loseText = document.querySelector('div .lose');
+const title = document.querySelector('span');
+
+let winCount = 0;
+let loseCount = 0;
+function game() {
+  let userChoice = this.getAttribute('id');
+  let result = playRound(userChoice, computerPlay());
+  if (result === 'Win') { winCount++; }
+  if (result === 'Lose') { loseCount++; }
+  winText.innerHTML = `<u>Wins</u><br>${winCount}`;
+  loseText.innerHTML = `<u>Losses</u><br>${loseCount}`;
+  if (winCount == 3) {
+    title.innerHTML = `<u>You won! Click a button to start again.</u>`;
+    winCount = 0; loseCount = 0;
+  }
+  if (loseCount == 3) {
+    title.innerHTML = `<u>You lost! Click a button to start again.</u>`;
+    winCount = 0; loseCount = 0;
+  }
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(e => {
+  e.addEventListener('click', game);
+});
+
 
